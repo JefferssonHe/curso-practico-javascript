a06e99a498ecb766c7dc412e78dff77cd07a9073
JefferssonHe <jefferssonsaar@gmail.com>
2022-04-18 16:04:26 -0500

    terminado

--- suma.js
+++ suma.js
@@ -1,19 +1,21 @@
-function suma(lado1,lado2){
+/* function suma(lado1,lado2){
     return parseInt(lado1+lado2);
 }
 function multi(lado1,lado2){
     return parseInt(lado1*lado2);
 }
 
+ */
+
 function calcularSuma(){
     var numero1 =parseInt(document.getElementById("NumeroUno").value);
     var numero2=parseInt(document.getElementById("NumeroDos").value);
 
 
-    var calculo=suma(numero1,numero2);
+    var calculo=numero1+numero2;
     alert(calculo);
     console.log(calculo);
-    document.writeln(calculo);
+
 }
 
 function calcularMultiplicacion(){
@@ -21,13 +23,43 @@ function calcularMultiplicacion(){
     var numero2=parseInt(document.getElementById("NumeroDos").value);
 
 
-    var calculo=multi(numero1,numero2);
+    var calculo=numero1*numero2;
     alert(calculo);
     console.log(calculo);
-    document.writeln(calculo);
+
 }
 
 
+function calcularRaiz(){
 
+    var numero1 =parseInt(document.getElementById("NumeroUno").value);
+var calculo=Math.sqrt(numero1).toFixed(2);
+alert(calculo);
+
+}
+
+function calcularDescuento(){
+var numero3=parseInt(document.getElementById("NumeroTres").value);
+var numero4=parseInt(document.getElementById("NumeroCuatro").value);
+
+const d=(numero3*(100-numero4)/100).toFixed(3);
+
+
+const respuesta=document.getElementById("respuesta");
+respuesta.innerText=d;
+
+
+
+
+alert(d);
+console.log({
+numero3,
+numero4,
+d
+});
+
+
+
+}
 
 
